e50467cfa53d7289009d36b67e1d74004ed34837
Yuxuan Shui <yshuiv7@gmail.com>
2014-06-15 23:22:11 +0800

    update plot

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-gnuplot -p -e "plot \"${1}_co\" with lines ,\"${1}_clo\" with lines ,\"${1}_so\" with lines, \"${1}_ci\" with lines, \"${1}_cli\" with lines;"
+gnuplot -p -e "set term postscript mono dashed eps; 
+	       set output '${1}.eps';
+	       set xlabel \"Time (hr)\";set ylabel \"Bandwidth (Kbit/s)\";
+	       plot \"${1}_co\" with linespoints title \"Cloud nodes outbound\",
+		    \"${1}_clo\" with linespoints title \"Client nodes outbound\",
+		    \"${1}_so\" with linespoints title \"Server outbound\",
+		    \"${1}_ci\" with linespoints title \"Cloud nodes inbound\",
+		    \"${1}_cli\" with linespoints title \"Client nodes inbound\";"
